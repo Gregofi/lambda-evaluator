02ed14e94d26f0a87722707b1871d820c8688733
Filip Gregor <filip.gregor98@gmail.com>
2022-10-28 22:51:39 +0200

    Add basic evaluation for basic LC

--- src/AST.rb
+++ src/AST.rb
@@ -1,41 +1,75 @@
 
 class Application
-    def initialize(exprs)
-        @expr = exprs
+    def initialize(function, argument)
+        @function = function
+        @argument = argument
     end
 
     def eval()
-
+        if @function.is_a? LambdaAbstraction
+            return @function.apply(@argument)
+        else
+            @argument.eval()
+            return self
+        end
     end
+
+    def replace(what, val)
+        @function.replace(what, val)
+        @argument.replace(what, val)
+    end
+
+    def to_s = "(#{@function.to_s} #{@argument.to_s})"
 end
 
 class LambdaAbstraction
-    def initialize(parameters, expr)
-        @parameters = parameters
+    # parameter is string, expr is Lambda expression
+    def initialize(parameter, expr)
+        @parameter = parameter
         @expr = expr
     end
 
-    def eval()
-
+    def apply(argval)
+        res = @expr.replace(@parameter, argval)
+        return res
     end
+
+    def eval()
+        expr.eval()
+        return self
+    end
+
+    def replace(what, val)
+        return if what == @parameter
+        @expr.replace(what, val)
+    end
+
+    def to_s = "(λ#{@parameter}. #{@expr.to_s})"
 end
 
 class Identifier
+    attr_reader :identifier
+
     def initialize(identifier)
         @identifier = identifier
     end
 
-    def eval()
+    def eval() = self
 
+    def replace(what, val)
+        raise ArgumentError.new("Expression to be replaced must be identifier, but is \"#{what}\"") if !what.is_a? Identifier
+        if identifier == what.identifier
+            return val
+        else
+            return self
+        end
     end
-end
 
-class Braces
-    def expr(expr)
-        @expr = expr
+    def to_s = @identifier.to_s
+
+    def eql?(other)
+        @identifier.eql?(other.identifier)
     end
 
-    def eval()
-        
-    end
+    def ==(other) = identifier == other.identifier
 end
